801dc2829cce32b7b86c6a5d6bc4f838641b1f85
Jihyun-Choi <034179@naver.com>
2023-11-29 19:44:25 +0900

    feat: add article viewset API

--- article/urls.py
+++ article/urls.py
@@ -1,9 +1,11 @@
+from article.views import ArticleViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 app_name = "article"
 
 router = DefaultRouter()
+router.register("", ArticleViewSet, basename="Article")
 
 urlpatterns = [
 ]
