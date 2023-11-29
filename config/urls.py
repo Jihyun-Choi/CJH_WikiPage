eab57a537e063268c622a16cdd30e794b26cd25a
Jihyun-Choi <034179@naver.com>
2023-11-29 17:09:17 +0900

    feat: initial setting

---
+++ config/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="WikiPage",
+        default_version='v1',
+        description="API description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="034179@naver.com"),
+        license=openapi.License(name="MIT LICENSE"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("admin/", admin.site.urls),
+    path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
